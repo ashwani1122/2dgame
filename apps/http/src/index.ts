4c6e5ac7c4b1d5870a37a560d90b50b28b152a87
ashwani1122 <ashwanisingh3846@gmail.com>
2025-08-23 22:58:02 +0530

    remove tokenfrontends

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import { router } from "./routes/v1/index.js";
 import cors from "cors";
 const app = express();
-const PORT = process.env.PORT || 3003;
+
+
+const PORT = 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
